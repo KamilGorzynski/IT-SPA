ee7cadd972ef7cf4775056a42e2db30c48075425
Kamil Local <kamil.gorzynski@onet.pl>
2020-03-16 17:07:16 +0100

    Date validation fix

--- src/rooms/addRoomToCart.js
+++ src/rooms/addRoomToCart.js
@@ -3,7 +3,6 @@ import { appCart } from '../it-spa';
 
 let dapArrDiff;
 
-
 function dateValidate(btn) {
     const arrival = new Date(btn.parentElement.childNodes[11].value);
     const departure = new Date(btn.parentElement.childNodes[13].value);
@@ -11,37 +10,45 @@ function dateValidate(btn) {
     const one_day = 1000 * 60 * 60 * 24
     
     dapArrDiff = Math.round(departure.getTime() - arrival.getTime()) / one_day;
-
-    if (arrival && arrival.getDate() - today.getDate() < 0) {
-        alert('Past arrival date');
+    if (!arrival.getDate() || !departure.getDate() ) {
+        alert('No arrival or departure date');
+        return false
     }
-    // jest blad na walidacji dat z nastepnego miesiaca
-    if (departure && departure.getDate() - today.getDate() < 0) {
-        alert('Past departure date');
+    if (arrival < today ) {
+        alert('Past or present arrival date');
+        return false
     }
-
-    if (arrival && departure && dapArrDiff > 365) {
-        alert('Over year');
+    if (departure < today < 0) {
+        alert('Past or present departure date');
+        return false
     }
+    if (arrival > departure) {
+        alert('Departre date lower then arrival');
+        return false
+    }
+    if (dapArrDiff > 365) {
+        alert('Over the year');
+        return false
+    }
+    return true
 }
 
 
 function addRoomToCart(){
-    dateValidate(this);
-
-    const childNodes = this.parentElement.childNodes;
-    const roomKey = childNodes[1].innerText
+    if (dateValidate(this)){
+        const childNodes = this.parentElement.childNodes;
+        const roomKey = childNodes[1].innerText
+        
+        const roomData = {
+            'amount': dapArrDiff,
+            'price': childNodes[3].childNodes[1].innerText,
+            'arrival': childNodes[11].value,
+            'departure': childNodes[13].value,
+        }
     
-
-    const roomData = {
-        'amount': dapArrDiff,
-        'price': childNodes[3].childNodes[1].innerText,
-        'arrival': childNodes[11].value,
-        'departure': childNodes[13].value,
+        appCart.addToCart(roomKey, roomData)
+        alert('Room added to Cart');
     }
-
-    appCart.addToCart(roomKey, roomData)
-    alert('Room added to Cart');
 }
 
 
