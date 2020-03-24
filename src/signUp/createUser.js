32bfa3982b52d70a5f8e17bfda16db4adcf82264
Kamil Local <kamil.gorzynski@onet.pl>
2020-03-24 06:34:48 +0100

    Registration valiation

--- src/signUp/createUser.js
+++ src/signUp/createUser.js
@@ -3,20 +3,40 @@ import { serviceInactive } from '../common/serviceInactive';
 
 const axios = require('axios').default;
 
-function makeUser() {
+function makeUserRequest(login, password, email) {
     axios.post('http://localhost:5000/users', {
-        "username": document.querySelector('.login').value,
-        "password": document.querySelector('.password').value,
-        "email": document.querySelector('.email').value
+        "username": login,
+        "password": password,
+        "email": email
     })
     .then(function (response) {  
        alert(response.data.details);
+       location.reload();
     })
     .catch(function (error) {
         document.querySelector('.signUpDiv').innerHTML = '';
         document.querySelector('.signUpDiv').append(serviceInactive());
     });
 }
+
+function makeUser() {
+    const login = document.querySelector('.login').value;
+    const password = document.querySelector('.password').value;
+    const confirmPassword = document.querySelector('.confirmPassword').value;
+    const email = document.querySelector('.email').value;
+
+    if (!login || !password || !confirmPassword || !email){
+        alert('Fill up all fields');
+        return;
+    } else if (confirmPassword !== password) {
+        alert('Different passwords');
+        return;
+    }
+
+    makeUserRequest(login, password, email);
+}
+
+
 
 export function createUser() {
     document.querySelector('.signUpBtn').addEventListener('click', makeUser);
