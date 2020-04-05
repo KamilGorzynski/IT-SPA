5da1837b3bf2e51d8156511d50e42f4b11d85829
Kamil Local <kamil.gorzynski@onet.pl>
2020-04-05 21:44:14 +0200

    Rwd treatments

--- src/treatments/treatmentCard.js
+++ src/treatments/treatmentCard.js
@@ -2,16 +2,18 @@ import $ from 'jquery';
 
 export const treatmentCard = (treatment) => {
     return $(`
-        <div class="cardTreatment">
-            <img src="${treatment.photo}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${treatment.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Area: ${treatment.area}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">Time: ${treatment.time}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">Price: <span>${treatment.price}</span> EUR</h6>
-            <p class="card-text">${treatment.description}</p>
-            <input type="number" max="10" min="1" value="1"></br>
-            <button type="button" class="AddToCart btn btn-secondary">Add to cart</button>
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="cardTreatment">
+                <img src="${treatment.photo}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${treatment.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Area: ${treatment.area}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Time: ${treatment.time}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Price: <span>${treatment.price}</span> EUR</h6>
+                <p class="card-text">${treatment.description}</p>
+                <input type="number" max="10" min="1" value="1"></br>
+                <button type="button" class="AddToCart btn btn-secondary">Add to cart</button>
+                </div>
             </div>
         </div>
     `)[0];
